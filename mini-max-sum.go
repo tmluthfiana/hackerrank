4d753821197fd0f039de8c6f7f2bb2982ebce452
trias.luthfiana <trias.luthfiana@eaciit.com>
2017-10-25 16:29:20 +0700

    mini max sum hackerrank

---
+++ mini-max-sum.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	//Mini-Max Sum
+	//Enter your code here. Read input from STDIN. Print output to STDOUT
+	scanner := bufio.NewScanner(os.Stdin)
+	lines := make([]string, 0)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+	var in []int
+	aa := strings.Split(lines[0], " ")
+	for _, a := range aa {
+		num, _ := strconv.Atoi(a)
+		in = append(in, num)
+	}
+
+	var a = make([]int, 5)
+	var sum int
+	sum = 0
+
+	for i := 0; i < len(a); i++ {
+		a[i] = in[i]
+		sum += a[i]
+	}
+
+	sort.Ints(a)
+	fmt.Println((sum - a[len(a)-1]), (sum - a[0]))
+
+}
